92451e4d68daa14bd0dde0379a394c7858294bae
Sabrina Lin <lsabrina@umich.edu>
2024-10-24 15:15:00 -0400

    Women's Team first draft done

--- generate_athlete_home_html.py
+++ generate_athlete_home_html.py
@@ -16,10 +16,9 @@ def is_valid_image(image_src, base_path):
     # Construct the full path to the image
     #full_image_path = os.path.join(os.path.dirname(base_path), image_src)
     full_image_path = image_src
-    print(full_image_path)
     return os.path.isfile(full_image_path)
 
-def generate_homepage(folder_path='mens_team', output_file='mens.html'):
+def generate_homepage(folder_path='womens_team', output_file='womens.html'):
     # Set to store unique links
     links = set()
 
@@ -31,17 +30,18 @@ def generate_homepage(folder_path='mens_team', output_file='mens.html'):
             image_src = extract_image_from_html(file_path)
             # Create a link with image if an image is found
             modified_string = re.sub('[0-9]+\.html', '', filename)
-            
-            mod_img_src = image_src.replace('../', '')
+
+            print(image_src)
+            if image_src is not None: 
+                mod_img_src = image_src.replace('../', '')
 
             if image_src and is_valid_image(mod_img_src, base_path = os.path.join(os.path.dirname(folder_path), mod_img_src)):
-                links.add(f'<li><a href="{filename}">{modified_string}<br><img src="{image_src}" alt="{modified_string}" style="width:100px;height:auto;"></a></li>')
+                links.add(f'<li><img src="{image_src}" alt="{modified_string}" style="width:100px;height:auto;"><br><a href="{"womens_teams/" + filename}">{modified_string}</a></li>')
             else: 
                 default = os.path.join(os.path.dirname(folder_path), "/images/profiles/default_image.jpg")
-                print(modified_string)
-                links.add(f'<li><a href="{filename}">{modified_string}<br><img src="{".."+default}" alt="{modified_string}" style="width:100px;height:auto;"></a></li>')
+                links.add(f'<li><a href="{filename}"><img src="{".."+default}" alt="{modified_string}" style="width:100px;height:auto;">{modified_string}</a></li>')
 
-    # Sort the links alphabetically
+     # Sort the links alphabetically
     sorted_links = sorted(links)
 
     # Create the HTML content for the homepage
@@ -51,11 +51,22 @@ def generate_homepage(folder_path='mens_team', output_file='mens.html'):
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <script src="https://kit.fontawesome.com/542a7b699c.js" crossorigin="anonymous"></script>
+    <link rel = "stylesheet" href = "css/reset.css">
+    <link rel = "stylesheet" href = "css/team_pages.css">
     <title>Women's Team Homepage</title>
 </head>
 <body>
+     <nav>
+        <ul>
+           <li><a href="index.html">Home Page</a></li>
+           <li><a href="mens.html">Men's Team</a></li>
+           <li><a href="womens.html">Women's Team</a></li>
+        </ul>
+    </nav>
+
     <h1>Welcome to the Women's Team Homepage</h1>
-    <ul>
+    <ul id = "all-athletes">
         {}
     </ul>
 </body>
